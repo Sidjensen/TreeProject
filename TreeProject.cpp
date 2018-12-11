1067605bf8fa31e9b83d25a8234bb224ae52d1cb
Sidjensen <sidney@jensenplanet.com>
2018-12-11 15:56:38 -0700

    Update TreeProject.cpp

--- TreeProject.cpp
+++ TreeProject.cpp
@@ -2,14 +2,19 @@
 //
 
 #include "stdafx.h"
-#include "BinaryTree.h"
 #include <string>
+#include <iostream>
 
 using namespace::std;
 
 int main()
 {
-	
+	int id, parent, lHash, rHash, lHist, rHist;
+	string rawdata;
+	cout << "enter a piece of raw data: ";
+	cin >> rawdata;
+	// Set to null, if first, some random thing for parent, and set Id from the data
+	// When new node is added, make parent id, and make new id
     return 0;
 }
 
@@ -20,4 +25,3 @@ int hash(string rawdata)
 	temp = temp * 37;
 	temp = temp % 10;
 }
-
