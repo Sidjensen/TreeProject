e4b7eb2b280ade742fd83de912aa8893f25eb28c
Sidjensen <sidney@jensenplanet.com>
2018-12-11 16:35:59 -0700

    Adding user use

--- TreeProject.cpp
+++ TreeProject.cpp
@@ -2,6 +2,8 @@
 //
 
 #include "stdafx.h"
+#include "Node.h"
+#include "LinkedList.h"
 #include <string>
 #include <iostream>
 
@@ -11,10 +13,40 @@ int main()
 {
 	int id, parent, lHash, rHash, lHist, rHist;
 	string rawdata;
-	cout << "enter a piece of raw data: ";
-	cin >> rawdata;
-	// Set to null, if first, some random thing for parent, and set Id from the data
-	// When new node is added, make parent id, and make new id
+	bool end = false;
+	while (end == false)
+	{
+		cout << "Type 'Q' to quit the program, 'S' to show all tree ID's, 'ID' to manipulate an ID,  or enter a piece of raw data: ";
+		cin >> rawdata;
+		if (rawdata == "Q")
+		{
+			end = true;
+		}
+		else if (rawdata == "S")
+		{
+			//show tree as a set of ID values
+		}
+		else if (rawdata == "ID")
+		{
+			string idTemp;
+			cout << "Type 'V' to see the entire record of the ID, and type 'U' to update the contents of the ID";
+			cin >> idTemp;
+			if (idTemp == "V")
+			{
+				// Show all records of the ID;
+			}
+			if (idTemp == "U")
+			{
+				//make the node have new contents, and probably change ID, and all parent stuff
+			}
+		}
+		else
+		{
+			// create new node on tree 
+			// Set to null, if first, some random thing for parent, and set Id from the data
+			// When new node is added, make parent id, and make new id
+		}
+	}
     return 0;
 }
 
