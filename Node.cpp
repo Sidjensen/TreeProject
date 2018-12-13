651255d43643f402e652fd3802e3c0efa98204f2
Sidjensen <sidney@jensenplanet.com>
2018-12-13 15:20:18 -0700

    Add stuff to functions

--- Node.cpp
+++ Node.cpp
@@ -45,16 +45,36 @@ Node::~Node()
 void Node::showIDs(string parent)
 {
 	// take parent ID and use to print out all other IDs
+	Node * Firstparent = parent_;
+	cout << ID;
+	while (leftchild_ != NULL)
+	{
+		// move to the left child
+		cout << ID;
+	}
 }
 
 void Node::showRecords(string id)
 {
 	// show all values stored at the id
+	cout << "ID: " << ID << endl;
+	cout << "Parent: " << PARENT << endl;
+	cout << "Raw Event: " << RAWE << endl;
+	cout << "Right Hash: " << RHASH << endl;
+	cout << "Left Hash: " << LHASH << endl;
+	cout << "Right Hash History: " << RHISTH << endl;
+	cout << "Left Hash History: " << LHISTH << endl;
+
 }
 
 void Node::changeNode(string id, string data)
 {
 	// change node data, then update the rest
+	// go to id
+	RAWE = data;
+	// change id
+	// change hash
+	// change hash history
 }
 
 void Node::newNode(string data)
